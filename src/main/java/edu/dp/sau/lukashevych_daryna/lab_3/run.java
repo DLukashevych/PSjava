8a8c59258081c905664126f265370a5844cab5e1
DLukashevych <llldasa9566@gmail.com>
2024-12-04 13:11:49 +0200

    Update run.java

--- src/main/java/edu/dp/sau/lukashevych_daryna/lab_3/run.java
+++ src/main/java/edu/dp/sau/lukashevych_daryna/lab_3/run.java
@@ -8,14 +8,7 @@ public class run {
     public static void main(String[] args) {
         UniversityCreator universityCreator = new UniversityCreator();
         University university = universityCreator.create();
-
-
-        //UniversityCreator universityCreator = new UniversityCreator();
-        //FacultyCreator facultyCreator = new FacultyCreator();
-        //DepartmentCreator departmentCreator = new DepartmentCreator();
-        //GroupCreator groupCreator = new GroupCreator();
-        //University university = universityCreator.create();
-        //UniversityPrinter printer = new UniversityPrinter();
-        //printer.printUniversityDetails(university)
+        UniversityPrinter printer = new UniversityPrinter();
+        printer.printUniversityDetails(university)
     }
 }
